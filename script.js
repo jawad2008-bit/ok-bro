4b6c0a8bdbd7bdd4f97a2543a290c1b5b36bb779
unknown <Administrator@LAB7-1.labs.org>
2026-01-08 10:41:04 +0500

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+// let image = document.querySelectorAll(".im")
+// function ChangeImages(){
+//     const newimages = ["burger2.avif","pizza2.avif","roll2.webp"]
+//     image.forEach((image,index) => {
+// image.src= newimages[index]
+//     });
+// }
+
+// ChangeImages()
